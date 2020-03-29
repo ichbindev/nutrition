3c17e08694ee78a6c7814625bc5e439ed26984c3
ichbindev <cmlinac@gmail.com>
2020-03-29 18:36:21 -0500

    more style changes on nutrition label

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NutritionLabel from "./components/NutritionLabel";
 import sample from "./sample.json";
 import "./App.css";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" render={() => <NutritionLabel nutritionInfo={sample} />}/>
+          <Route exact path="/" render={() => <Home nutritionInfo={sample} />}/>
           <Route path="*" render={() => `Page not found`}/>
         </Switch>
       </Router>

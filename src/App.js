dde8360f9c03c7ad0ed04f467268749789bb4249
Chris Mlinac <chrismlinac@gmail.com>
2020-04-01 21:30:20 -0500

    add navbar component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import sample from "./sample.json";
 import "./App.css";
-import Home from './pages/Home';
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Switch>
-          <Route exact path="/" render={() => <Home nutritionInfo={sample} />}/>
-          <Route path="*" render={() => `Page not found`}/>
+          <Route
+            exact
+            path="/"
+            render={() => <Home nutritionInfo={sample} />}
+          />
+          <Route path="*" render={() => `Page not found`} />
         </Switch>
       </Router>
-      
     </div>
   );
 }

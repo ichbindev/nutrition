949d9939ee302d93a1a1b6229dbebee218cca5d3
ichbindev <cmlinac@gmail.com>
2020-04-11 18:46:34 -0500

    css is hard so I went to reactstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import sample from "./sample.json";
 import "./App.css";
 import Home from "./pages/Home";
+import Log from "./pages/Log";
+import Recipes from "./pages/Recipes";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -15,6 +17,16 @@ function App() {
             exact
             path="/"
             render={() => <Home nutritionInfo={sample} />}
+          />
+          <Route
+            exact
+            path="/log/"
+            render={() => <Log nutritionInfo={sample} />}
+          />
+          <Route
+            exact
+            path="/recipes/"
+            render={() => <Recipes nutritionInfo={sample} />}
           />
           <Route path="*" render={() => `Page not found`} />
         </Switch>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar as ReactStrapNavbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div>
-      Navbar
+      <ReactStrapNavbar color="light" light expand="md">
+        <NavbarBrand href="/">nutrition</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/log/">log</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/recipes/">recipes</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Food is made of stuff</NavbarText>
+        </Collapse>
+      </ReactStrapNavbar>
     </div>
-  )
+  );
 }
 
 export default Navbar;

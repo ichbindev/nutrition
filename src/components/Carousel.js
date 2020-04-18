c8793b768132305f6fe3679c1f33556444d2d5ef
ichbindev <cmlinac@gmail.com>
2020-04-17 21:54:51 -0500

    added carousel, needs styling baaaad

---
+++ src/components/Carousel.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import {
+  Carousel as RSCarousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from "reactstrap";
+import { HOME, homeCarouselItems } from "../utilities/consts";
+
+const getItemsFromType = type => {
+  switch (type) {
+    case HOME: 
+      return homeCarouselItems;
+    default:
+      return [];
+  }
+}
+
+const Carousel = ({ type }) => {
+  const items = getItemsFromType(type);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = newIndex => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map(item => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <RSCarousel activeIndex={activeIndex} next={next} previous={previous}>
+      <CarouselIndicators
+        items={items}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
+    </RSCarousel>
+  );
+};
+
+export default Carousel;

24d4e9a2ddafdc65ca4e5e8c04fce0d61e015c72
ichbindev <cmlinac@gmail.com>
2020-04-19 16:20:46 -0500

    simulate network request in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import NutritionLabel from "../components/NutritionLabel";
 import Carousel from "../components/Carousel";
 import SearchBar from "../components/Searchbar";
 import { HOME } from "../utilities/consts";
 
 const Home = ({ nutritionInfo }) => {
+  const [food, setFood] = useState(null);
+
+  // simulate a network request until backend
+  const onSearch = term => {
+    const idx = Math.floor(Math.random() * nutritionInfo.length);
+    console.log(idx);
+    setTimeout(() => setFood(nutritionInfo[idx]), 450);
+  };
+
   return (
     <div>
       <Carousel type={HOME} />
-      <SearchBar />
-      {nutritionInfo.map(food => (
-        <NutritionLabel key={food.name} nutritionInfo={food} />
-      ))}
+      <SearchBar onSearch={onSearch}/>
+      {food && <NutritionLabel nutritionInfo={food} />}
     </div>
   );
 };

8e9fed8d1a8a07c367e9686bf3a4f4955f63eb20
cmlinac <cmlinac@gmail.com>
2020-05-07 21:11:46 -0500

    set up more reducer ideas

--- src/utilities/reducer.js
+++ src/utilities/reducer.js
@@ -2,15 +2,21 @@ export default function(state, action) {
   switch (action.type) {
     case LOG_ADD_FOOD:
       return {...state};
-    case LOG_REMOVE_FOOD:
+    case LOG_DELETE_FOOD:
       return {...state};
     case ADD_RECIPE:
       return {...state};
+    case SET_GOAL:
+      return {...state};
+    case SET_TDEE:
+      return {...state};
     default:
-      return state;
+      return {...state};
   }
 }
 
 const LOG_ADD_FOOD = "LOG_ADD_FOOD";
-const LOG_REMOVE_FOOD = "LOG_REMOVE_FOOD";
+const LOG_DELETE_FOOD = "LOG_REMOVE_FOOD";
 const ADD_RECIPE = "ADD_RECIPE";
+const SET_GOAL = "SET_GOAL";
+const SET_TDEE = "SET_TDEE"

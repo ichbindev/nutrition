732646035313d7d5a2b3a08bc97acabefd0ebef6
cmlinac <cmlinac@gmail.com>
2020-05-18 21:56:30 -0500

    start refactor of nutritionLog

--- src/components/NutritionLog.js
+++ src/components/NutritionLog.js
@@ -6,7 +6,40 @@ const removeLoggedItem = (e) => {
   console.log(e.target);
 };
 
-const NutritionLog = ({ userNutritionForDay }) => {
+const foodData = [
+  {
+    name: 'Egg', 
+    meal: 'Breakfast',
+    calories: 70,
+    id: 1
+  },
+  {
+    name: 'Lunch', 
+    meal: 'Rice',
+    calories: 120,
+    id: 2
+  },
+  {
+    name: 'Beans', 
+    meal: 'Lunch',
+    calories: 80,
+    id: 3
+  },
+  {
+    name: 'Hamburger', 
+    meal: 'Dinner',
+    calories: 520,
+    id: 4
+  },
+  {
+    name: 'Fries', 
+    meal: 'Dinner',
+    calories: 300,
+    id: 5
+  }
+];
+
+const NutritionLog = ({ userNutritionForDay = foodData}) => {
   return (
     <div className="log-table-container">
       <Table className="log-table">
@@ -19,46 +52,16 @@ const NutritionLog = ({ userNutritionForDay }) => {
           </tr>
         </thead>
         <tbody>
+        {userNutritionForDay.map(food => 
           <tr>
-            <th scope="row">Breakfast</th>
-            <td>Egg</td>
-            <td>70</td>
+            <th scope="row">{food.meal}</th>
+            <td>{food.name}</td>
+            <td>{food.calories}</td>
             <td>
-              <button foodName={"Egg"} onClick={(e) => removeLoggedItem(e)}>x</button>
+              <button id={food.id} onClick={(e) => removeLoggedItem(e)}>x</button>
             </td>
           </tr>
-          <tr>
-            <th scope="row">Lunch</th>
-            <td>Rice</td>
-            <td>120</td>
-            <td>
-              <button onClick={(e) => removeLoggedItem(e)}>x</button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Lunch</th>
-            <td>Beans</td>
-            <td>80</td>
-            <td>
-              <button onClick={(e) => removeLoggedItem(e)}>x</button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Dinner</th>
-            <td>Hamburger</td>
-            <td>520</td>
-            <td>
-              <button onClick={(e) => removeLoggedItem(e)}>x</button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Dinner</th>
-            <td>Fries</td>
-            <td>300</td>
-            <td>
-              <button onClick={(e) => removeLoggedItem(e)}>x</button>
-            </td>
-          </tr>
+        )};
         </tbody>
       </Table>
     </div>

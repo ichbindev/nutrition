25da68876a99640ccce240086befa60229a09b0d
cmlinac <cmlinac@gmail.com>
2020-05-26 22:38:05 -0500

    restructure log page to divide by meals

--- src/pages/Log.js
+++ src/pages/Log.js
@@ -1,10 +1,49 @@
 import React from "react";
 import NutritionLog from "../components/NutritionLog";
 
+const sampleData = {
+  breakfast: [{
+    name: 'Egg', 
+    calories: 70,
+    amount: '40g',
+    id: 1
+  }],
+  lunch: [{
+    name: 'Rice', 
+    calories: 120,
+    amount: '100g',
+    id: 2
+  },
+  {
+    name: 'Beans', 
+    calories: 80,
+    amount: '120g',
+    id: 3
+  }],
+  dinner: [{
+    name: 'Hamburger', 
+    calories: 520,
+    amount: '500g',
+    id: 4
+  },
+  {
+    name: 'Fries', 
+    calories: 300,
+    amount: '250g',
+    id: 5
+  }],
+  snacks: [{
+    name: 'Grapes', 
+    calories: 50,
+    amount: '100g',
+    id: 6
+  }],
+};
+
 const Log = () => {
   return (
     <div>
-      <NutritionLog userNutrionForDay={`TODO`} />
+      <NutritionLog userNutritionForDay={sampleData} />
     </div>
   );
 };
